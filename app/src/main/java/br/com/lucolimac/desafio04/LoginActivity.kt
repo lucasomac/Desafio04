61f59b25eff12de8209b264b9a61551563e9d7a8
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-01-24 18:11:53 -0300

    Conclusão do login, inserção da Register

--- app/src/main/java/br/com/lucolimac/desafio04/LoginActivity.kt
+++ app/src/main/java/br/com/lucolimac/desafio04/LoginActivity.kt
@@ -1,14 +1,32 @@
 package br.com.lucolimac.desafio04
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.com.lucolimac.desafio04.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.includeLoginFields.tvCreateAccouont.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    RegisterActivity::class.java
+                )
+            )
+        }
+    }
+
+    override fun onClick(v: View?) {
+//        when (v!!.id) {
+//            R.id.tvCreateAccouont -> {
+//                startActivity(Intent(this, RegisterActivity::class.java))
+//            }
+//        }
     }
 }
--- app/src/main/java/br/com/lucolimac/desafio04/MainActivity.kt
+++ app/src/main/java/br/com/lucolimac/desafio04/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setSupportActionBar(findViewById(R.id.toolbar))
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
